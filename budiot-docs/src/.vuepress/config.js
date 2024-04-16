1a7e9e1a2a2c1a40802b1847dee500818435b6af
wizzer <wizzer@qq.com>
2024-04-16 22:28:51 +0800

    update: docs

---
+++ budiot-docs/src/.vuepress/config.js
@@ -0,0 +1,77 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'BudIot',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+      {
+        text: '演示',
+        link: 'https://demo.budiot.com'
+      },
+      {
+        text: '捐赠',
+        link: 'https://budwk.com/donation'
+      },
+      {
+        text: '源码',
+        items: [
+          { text: 'Gitee', link: 'https://gitee.com/budwk/budiot' },
+          { text: 'Github', link: 'https://github.com/budwk/budiot' }
+        ]
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '指南',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
